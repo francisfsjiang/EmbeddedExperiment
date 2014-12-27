b75d80b0f9148775642861c6500d012630366922
neveralso <neveralso@gmail.com>
2014-12-27 16:09:33 +0800

    commit

---
+++ src/socket_client.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include<sys/socket.h>
+#include<sys/un.h>
+
+//#pargma pack(1)
+#define SOCKET_FILE_PATH "/tmp/ipc_test.sock"
+
+static char data_buffer[256] = {1, 1, 2, 3, 5, 8, 13, 21};
+
+struct test_s
+{
+    char a;
+    int b;
+    int c;
+}test_s;
+
+int main() {
+    int socket_fd;
+    int ret = 0;
+    int i;
+    static struct sockaddr_un sock_addr;
+    printf("socket client start.\n");
+
+    socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    printf("connect fd: %d\n",socket_fd);
+
+    if(socket_fd < 0){
+        perror("socket create failed.\n");
+        exit(-1);
+    }
+    else
+    {
+        sock_addr.sun_family = AF_UNIX;
+        strcpy(sock_addr.sun_path, SOCKET_FILE_PATH);
+        ret = connect(socket_fd, (struct sockaddr*)&sock_addr, sizeof(sock_addr));
+        if(ret < 0)
+        {
+            close(socket_fd);
+            printf("connect failed\n");
+            exit(-1);
+        }
+        else
+        {
+            write(socket_fd, data_buffer, 16);
+            memset(data_buffer, 0, sizeof(data_buffer));
+            read(socket_fd, data_buffer, sizeof(data_buffer));
+            for(i = 0;i < 16;i++)
+            {
+                printf("%d ", data_buffer[i]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
---
+++ src/socket_server.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include<sys/socket.h>
+#include<sys/un.h>
+
+//#pargma pack(1)
+#define SOCKET_FILE_PATH "/tmp/ipc_test.sock"
+
+static char data_buffer[256] = {0, 1, 2, 3, 4, 5, 6, 7};
+
+
+int main()
+{
+    int socket_fd;
+    int coon;
+    ssize_t recv_num;
+    int ret = 0;
+    int i;
+    static struct sockaddr_un sock_addr;
+    printf("socket client start.\n");
+
+
+
+    socket_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    printf("connect fd: %d\n",socket_fd);
+
+    if(socket_fd < 0){
+        perror("socket create failed.\n");
+        unlink(SOCKET_FILE_PATH);
+        exit(-1);
+    }
+    else
+    {
+        sock_addr.sun_family = AF_UNIX;
+        strcpy(sock_addr.sun_path, SOCKET_FILE_PATH);
+        ret = bind(socket_fd, (struct sockaddr*)&sock_addr, sizeof(sock_addr));
+        chmod(SOCKET_FILE_PATH, 0777);
+        if(ret < 0)
+        {
+            close(socket_fd);
+            printf("bind failed\n");
+            unlink(SOCKET_FILE_PATH);
+            exit(-1);
+        }
+        ret = listen(socket_fd, 0);
+        if (ret < 0 )
+        {
+            close(socket_fd);
+            printf("listen failed.\n");
+            unlink(SOCKET_FILE_PATH);
+            exit(-1);
+        }
+        while (1)
+        {
+            coon = accept(socket_fd, NULL, 0);
+            if(coon < 0)
+            {
+                printf("accept socket failed.\n");
+                unlink(SOCKET_FILE_PATH);
+                exit(-1);
+            }
+            memset(data_buffer, 0, sizeof(data_buffer));
+            recv_num  = read(coon, data_buffer, sizeof(data_buffer));
+            printf("received---------------\n");
+            for(i = 0;i < recv_num; ++i)
+            {
+                printf("%d ", data_buffer[i]);
+                data_buffer[i] *= 2;
+            }
+            write(coon, data_buffer, (size_t)recv_num);
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
